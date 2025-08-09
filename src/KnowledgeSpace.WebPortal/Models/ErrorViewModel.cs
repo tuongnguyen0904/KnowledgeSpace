f74d138d831180061e82a3d349ea103b6b207cfa
nguyentrongtuong <myproject0904@gmail.com>
2025-08-09 18:41:53 +0700

    first commit

---
+++ src/KnowledgeSpace.WebPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace KnowledgeSpace.WebPortal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
